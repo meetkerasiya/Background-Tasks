f417976e6e0a8eb40342f46380de983edf086d93
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-23 14:58:04 +0530

    first commit

---
+++ MyBackgroundService.cs
@@ -0,0 +1,10 @@
+﻿namespace BackgroundTasks;
+
+public class MyBackgroundService : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine(("From MyBackgroundService: ExecuteAsync"));
+        return Task.CompletedTask;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using BackgroundTasks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var services = builder.Services;
+//for bg services
+services.AddHostedService<MyBackgroundService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
