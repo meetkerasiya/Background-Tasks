480a3515e51b27d347dd708b02d0c9da81776fb3
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-23 15:12:15 +0530

    overridden the methods for hosted service

--- MyBackgroundService.cs
+++ MyBackgroundService.cs
@@ -2,9 +2,24 @@
 
 public class MyBackgroundService : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    private readonly ILogger<MyBackgroundService> _logger;
+
+    public MyBackgroundService(ILogger<MyBackgroundService> logger)
     {
-        Console.WriteLine(("From MyBackgroundService: ExecuteAsync"));
-        return Task.CompletedTask;
+        _logger = logger;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("From MyBackgroundService: ExecuteAsync {dateTime}",DateTime.Now);
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+        }
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("From MyBackgroundService: StopAsync {dateTime}",DateTime.Now);
+        return base.StopAsync(cancellationToken);
     }
 }
